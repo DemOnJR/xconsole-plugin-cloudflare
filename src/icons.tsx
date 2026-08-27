ca57f33590d57bc864fff6800db5e854c0c441db
Pipiasi Bogdan <pipiasibogdan@gmail.com>
2026-08-28 01:21:08 +0200

    feat: add cloudflare_list_zones and cloudflare_get_zone_analytics tools and svg iconography

--- src/icons.tsx
+++ src/icons.tsx
@@ -769,3 +769,13 @@ export function HistoryIcon(props: IconProps) {
   );
 }
 
+export function ChartIcon(props: IconProps) {
+  return (
+    <svg {...base(props)}>
+      <line x1="18" y1="20" x2="18" y2="10" />
+      <line x1="12" y1="20" x2="12" y2="4" />
+      <line x1="6" y1="20" x2="6" y2="14" />
+    </svg>
+  );
+}
+
